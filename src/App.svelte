de49c43acbfe73682a02b04c2b1c6654ca51b692
Lucas Kolinger <lucaskolinger@icloud.com>
2023-02-23 14:39:24 +0100

    Finishing touches

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,9 @@
 <script>
   import Icon from "@iconify/svelte";
 
-  let isCloseCrossShown = false;
-  const handleCloseBtnEnter = () => {
-    isCloseCrossShown = true;
-  };
-  const handleCloseBtnLeave = () => {
-    isCloseCrossShown = false;
-  };
-
   let mailInputElement;
   let textInputElement;
-  let formElement;
+  let nameInputElement;
   let showMailInput = false;
   let showTextInput = false;
   let showMailError = false;
@@ -19,6 +11,10 @@
   let showNameError = false;
 
   let message = { name: "", email: "", text: "" };
+
+  setTimeout(() => {
+    nameInputElement.focus();
+  }, 1);
 
   const onNameSubmit = () => {
     let nameIsValid = validateName();
@@ -74,53 +70,44 @@
     }
   };
 
-  // const handleSubmit = () => {
-  //   let textIsValid = validateText();
-  //   if (textIsValid) {
-  //     const formData = new FormData(formElement);
-
-  //     fetch("/", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //       body: new URLSearchParams(formData.toString()),
-  //     })
-  //       .then(() => console.log("Form successfully submitted"))
-  //       .catch((error) => alert(error));
-  //   } else {
-  //     showTextError = true;
-  //   }
-  // };
+  const handleSubmit = () => {
+    let textIsValid = validateText();
+    if (textIsValid && !showMailError && !showTextError) {
+      console.log(message);
+      alert("You submitted your message!");
+      location.reload();
+    } else {
+      showTextError = true;
+    }
+  };
 </script>
 
 <article class="terminal-card">
   <div class="controls-row">
-    <button
-      on:mouseover={handleCloseBtnEnter}
-      on:focus={handleCloseBtnEnter}
-      on:mouseleave={handleCloseBtnLeave}
-      class="system-btn close"
-    >
-      {#if isCloseCrossShown}
-        <Icon width="16" height="16" icon="material-symbols:close-rounded" />
-      {/if}
-    </button>
+    <button class="system-btn close" />
     <button class="system-btn yellow" />
     <button class="system-btn green" />
-    <p class="top-line">~contact lucas kolinger</p>
+    <p class="top-line">~ Contact Lucas Kolinger</p>
   </div>
 
-  <form
-    data-netlify="true"
-    bind:this={formElement}
-    name="contact"
-    method="POST"
-  >
+  <form name="contact" on:submit|preventDefault={handleSubmit}>
     <input type="hidden" name="form-name" value="contact" />
     <div class="name-input">
-      <p>~what's your name?</p>
-      <input type="text" name="name" bind:value={message.name} autofocus />
-      <div on:click={onNameSubmit}>
-        <Icon icon="icon-park-solid:enter-key-one" />
+      <p class="prompt">~ What's your name?</p>
+      <div class="input-and-enter">
+        <input
+          type="text"
+          name="name"
+          bind:value={message.name}
+          bind:this={nameInputElement}
+        />
+        <div class="enter" on:click={onNameSubmit} on:keyup={onNameSubmit}>
+          <Icon
+            color="hsl(349, 50%, 50%)"
+            width="1.3rem"
+            icon="icon-park-solid:enter-key-one"
+          />
+        </div>
       </div>
     </div>
     {#if showNameError}
@@ -128,15 +115,21 @@
     {/if}
     {#if showMailInput}
       <div class="mail-input">
-        <p>~what's your e-mail?</p>
-        <input
-          type="email"
-          name="mail"
-          bind:this={mailInputElement}
-          bind:value={message.email}
-        />
-        <div on:click={onMailSubmit}>
-          <Icon icon="icon-park-solid:enter-key-one" />
+        <p class="prompt">~ What's your e-mail?</p>
+        <div class="input-and-enter">
+          <input
+            type="email"
+            name="mail"
+            bind:this={mailInputElement}
+            bind:value={message.email}
+          />
+          <div class="enter" on:click={onMailSubmit} on:keyup={onMailSubmit}>
+            <Icon
+              color="hsl(349, 50%, 50%)"
+              width="1.3rem"
+              icon="icon-park-solid:enter-key-one"
+            />
+          </div>
         </div>
       </div>
       {#if showMailError}
@@ -148,9 +141,9 @@
 
     {#if showTextInput}
       <div class="text-input">
-        <p>~how can I help you?</p>
+        <p class="prompt">~ How can I help you?</p>
         {#if showTextError}
-          <div class="error">Please write a bit more</div>
+          <div class="text-error">Please write a bit more</div>
         {/if}
         <textarea
           name="Text"
@@ -160,26 +153,29 @@
           bind:this={textInputElement}
         />
       </div>
-      <button type="submit">Submit</button>
+      <button class="submit-button" type="submit">Send message</button>
     {/if}
   </form>
 </article>
 
 <style>
   .terminal-card {
-    background-color: purple;
-    width: 30rem;
-    height: 30rem;
-    border-radius: 1rem;
+    background-color: var(--terminal-color);
+    width: 80vw;
+    max-width: 40rem;
+    min-height: 30rem;
+    border-radius: 0.5rem;
+    border: 0.25px solid hsl(0, 0%, 25%);
   }
   .controls-row {
-    height: 3rem;
+    height: 3.5rem;
     display: flex;
     align-items: center;
     gap: 0.5rem;
-    background-color: gray;
-    border-top-left-radius: 1rem;
-    border-top-right-radius: 1rem;
+    color: var(--text-color);
+    background-color: var(--top-bar-color);
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
     padding-inline: 1rem;
   }
   .system-btn {
@@ -189,37 +185,52 @@
     border: none;
   }
 
+  .top-line {
+    margin-left: 1rem;
+  }
+
   .yellow {
-    background-color: yellow;
+    background-color: hsl(60, 50%, 50%);
   }
   .close {
-    background-color: red;
-    /* display: flex;
-    justify-content: center;
-    align-items: center; */
+    background-color: hsl(0, 50%, 50%);
   }
+
   .green {
-    background-color: green;
+    background-color: hsl(90, 50%, 50%);
   }
 
   .name-input,
   .mail-input {
     display: flex;
-    align-items: center;
-    gap: 1rem;
-    margin-left: 2rem;
+    flex-direction: column;
+    margin-inline: 2rem;
   }
 
-  .text-input {
+  .name-input {
+    margin-top: 1rem;
+  }
+
+  .prompt {
+    color: var(--text-color);
+  }
+
+  .input-and-enter {
     display: flex;
-    flex-direction: column;
+    align-items: center;
     gap: 1rem;
-    margin-left: 2rem;
   }
 
   input {
-    background-color: transparent;
     border: none;
+    padding-left: 1rem;
+    font-family: "Source Code Pro", monospace;
+    width: 80%;
+    color: var(--input-color);
+    font-size: 1rem;
+    background-color: var(--top-bar-color);
+    border-radius: 1rem;
+    height: 2.5rem;
   }
 
   input:focus {
@@ -228,7 +239,65 @@
     outline: none;
   }
 
+  .text-input {
+    display: flex;
+    flex-direction: column;
+    margin-inline: 2rem;
+    max-width: 25rem;
+  }
+
+  .text-input textarea {
+    background-color: var(--top-bar-color);
+    font-family: "Source Code Pro", monospace;
+    font-size: 1rem;
+    color: var(--input-color);
+    border-radius: 0.5rem;
+    padding: 1rem;
+    outline: none;
+    border: none;
+    caret-color: var(--input-color);
+  }
+
   .error {
-    color: red;
+    margin-inline: 2rem;
+    margin-bottom: 1rem;
+    margin-top: 0.5rem;
+    color: var(--input-color);
+  }
+
+  .text-error {
+    color: var(--input-color);
+    margin-bottom: 1rem;
+  }
+
+  .submit-button {
+    border-radius: 1rem;
+    border: none;
+    margin-top: 2rem;
+    padding-inline: 1rem;
+    padding-block: 0.5rem;
+    background-color: var(--text-color);
+    color: var(--input-color);
+    margin-left: 2rem;
+    font-family: "Source Code Pro", monospace;
+    margin-bottom: 2rem;
+  }
+
+  .submit-button:hover {
+    background-color: hsl(349, 50%, 55%);
+  }
+
+  @media screen and (min-width: 560px) {
+    .name-input,
+    .mail-input {
+      flex-direction: row;
+      align-items: center;
+    }
+    .input-and-enter {
+      width: 100%;
+    }
+    .prompt {
+      width: 25rem;
+    }
   }
 </style>
